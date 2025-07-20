38cb770be8aaca5e92f0235b21231a48ec65a8a8
Lokesh Sharma <138589391+lokeshsharma19@users.noreply.github.com>
2025-07-20 14:15:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (37.99%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,9 +1,44 @@
 class Solution {
 public:
-    bool rotateString(string s, string goal) {
-        if(s.size()!= goal.size()) return false;
-        string newS = s + s;
-        return newS.find(goal) != string::npos;
+    vector<int> buildLPS(string& pattern){
+        int len = 0;
+        int n = pattern.size();
+        vector<int> lps(n,0);
+        for(int i=1;i<n;){
+            if(pattern[len] == pattern[i]){
+                len++;
+                lps[i] = len;
+                i++;
+            }else{
+                if(len > 0){
+                    len = lps[len-1];
+                }else{
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
 
+    bool rotateString(string s, string goal) {
+        int m = s.size();
+        if(m!= goal.size()) return false;
+        vector<int> lps = buildLPS(goal);
+        int j = 0;
+        for(int i =0; i<2*m;){
+            int index = i%m;
+            if(goal[j] == s[index]){
+                j++;
+                i++;
+            }
+            else {
+                if(j>0){
+                    j = lps[j-1];
+                }else i++;
+            }
+            if(j==goal.size()) return true;
+        }
+        return false;
     }
 };
